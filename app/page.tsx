83f94ca1d29b26d4d8d24c8d9d37a868409ec7f4
JeffY1710 <jeff.yabas@gmail.com>
2024-04-18 09:49:40 +0200

    almost finished crud

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import Image from "next/image";
-import { getUser } from "./database/User/crud";
-
-
 
 export default async function Home() {
-  const test = await getUser();
-  console.log(test);
-  
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
