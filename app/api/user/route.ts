d7be9916474344d6b363fe19a4ad4936663573d8
SiERRA74 <aksierra74@gmail.com>
2024-04-18 16:38:17 +0200

    api in progress

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,18 +1,29 @@
 
 import { StatusCodes } from "http-status-codes";
-import { getAllUser } from "@/app/database/User/crud";
+import { getAllUser,getUser,createUser,deleteUser,updateUser} from "@/app/database/User/crud";
 
 const GET = async () => {
     const users = await getAllUser();
-    console.log(users.map(user => user.name));
-    return Response.json({message: "API USERS : GET", users: users.map((user) => {name: user.name, email: user.email}))};
+    console.log(users);
+    const usersInfo = users.map(user => ({id: user.id_user, name: user.name, email: user.email, phone : user.phone_number, role : user.role_id, created_at : user.created_at, updated_at : user.updated_at}));
+    return Response.json({
+        message: "API USERS : GET",
+        users: usersInfo
+    });
 }
 
-const POST = () => {
+
+const POST = async (req: Request) => {
+    const data = await req.json();
+    createUser(data);
     return Response.json({message: "API USERS : POST"});
 }
 
-const DELETE = () => {
+
+const DELETE = async (req:Request) => {
+    const id_user = await req.json();
+
+    deleteUser(id_user.id);
     return Response.json({message: "API USERS : DELETE"});
 }
 const PUT = () => {
